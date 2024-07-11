5d71e337f033cdbd45ddacd9c0d311759b43346d
James Wyatt <jamesrwyatt35@gmail.com>
2024-07-11 11:14:49 -0400

    update next js config for production build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    output: 'export',
+    eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+    },
+    trailingSlash: true, // Resolve uri issue with routing to '/path' when server is expecting '/path.html'
+    }
+    
+    
+    
+    module.exports = nextConfig;
